0b5e72dbb98881fcb0f58fc15c40d104753ab74a
sonalmkr <sonalmkr@gmail.com>
2021-03-20 15:55:39 +0530

    worksheet

--- src/pages/ArticlePageNew.js
+++ src/pages/ArticlePageNew.js
@@ -11,7 +11,7 @@ function ArticlePageNew ({articleName})  {
     const articles = [
         {
             name: 'React',
-            title: 'Welcome to School Life!!',
+            title: 'Welcome to React!!',
             content:  `React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
 Declarative views make your code more predictable and easier to debug. Build encapsulated components that manage their own state, then compose them to make complex UIs.
 Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.`,
@@ -50,18 +50,7 @@ Since component logic is written in JavaScript instead of templates, you can eas
         </>
       );
 
-   /* const matchingArticle = articles.find(article => article.name === 'School');
-    return matchingArticle ? 
-    (
-        <>
-        <h1>{matchingArticle.title}</h1>
-        <UpvotesSection upvotes={articleInfo.upvotes} articleName={name} setArticleInfo={setArticleInfo} />
-        {matchingArticle.content.map((paragraph, key) => <p key={key}>{paragraph}</p>)}
-        <CommentsList comments={articleInfo.comments} articleName={name} setArticleInfo={setArticleInfo} />
-        </>
-    ) : (
-        <h1>Uh oh, looks like that article doesn't exist</h1>
-    );*/
+   
 };
 
 export default ArticlePageNew;
